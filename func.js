34c8b63aab0c77aa46f8b2cfe1c918f8041e37b2
Astolto122 <89078896+Astolto122@users.noreply.github.com>
2022-08-02 10:46:11 +0200

    Filtro per categoria
    In questo commit è stata aggiunta la capacità di filtrare il documento per categoria tramite un box apposito

--- func.js
+++ func.js
@@ -58,7 +58,7 @@ function play(btn) {
     box.firstElementChild.firstElementChild.lastElementChild.style.display = "inline";//mostro il bottone pause
     box.lastElementChild.style.display = "none";//nascondo il minimize
 
-    box.children[1].insertAdjacentElement("afterend",render(text + "\n"));//inserimento degli oggetti renderizzati
+    box.children[1].insertAdjacentElement("afterend", render(text + "\n"));//inserimento degli oggetti renderizzati
 
 }
 
@@ -76,7 +76,7 @@ function pause(btn) {
 
 function delContent(btn) {
     let box = btn.parentElement.parentElement.parentElement;
-    if(box.lastElementChild.className!=="reduce"){
+    if (box.lastElementChild.className !== "reduce") {
         box.children[1].innerText = "";
     }
 }
@@ -90,15 +90,15 @@ function minimize(btn) {
     let editBox = btn.parentElement.children[1];
     if (editBox.style.height !== "64px") {
         editBox.style.height = "64px";
-        btn.innerHTML=`<i class="bi bi-chevron-compact-down"></i>`;
+        btn.innerHTML = `<i class="bi bi-chevron-compact-down"></i>`;
     } else {
         //console.log(editBox.scrollHeight);
-        editBox.style.height="auto";
-        btn.innerHTML=`<i class="bi bi-chevron-compact-up"></i>`
+        editBox.style.height = "auto";
+        btn.innerHTML = `<i class="bi bi-chevron-compact-up"></i>`
     }
 }
 
-function add(content) {
+function add(category, content) {
     let parent = document.getElementById("app");
     parent.insertAdjacentHTML("beforeend", `
     <div class="box">
@@ -107,8 +107,8 @@ function add(content) {
             <i class="optIcon bi bi-play" onclick="play(this)"></i>
             <i class="optIcon bi bi-pause" onclick="pause(this)" style="display:none;"></i>
         </span>
-        &nbsp
         <span class="rightOpt">
+            <input class="inputBox" name="category" value="${category}">
             <i class="optIcon bi bi-eraser" onclick="delContent(this)"></i>
             <i class="optIcon bi bi-x-circle warning" onclick="delBox(this)"></i>
         </span>
@@ -119,17 +119,76 @@ function add(content) {
     `);
 }
 
+function setFilter(selector) {
+    let app = document.getElementById("app").children;
+
+    for (let i = 0; i < app.length; i++) {
+        app[i].style.display = "block";
+    }
+
+    for (let i = 0; i < app.length; i++) {
+        if (app[i].firstElementChild.lastElementChild.firstElementChild.value !== selector.value) {
+            app[i].style.display = "none";
+        }
+        if (selector.value === "") {
+            app[i].style.display = "block";
+        }
+    }
+    document.getElementById("category").remove();
+}
+
+function filterDoc(btn) {
+    let app = document.getElementById("app").children;
+    let categories = [];
+
+    for (let i = 0; i < app.length; i++) {
+        categories.push(app[i].firstElementChild.lastElementChild.firstElementChild.value);
+    }
+
+    categories = new Set(categories);
+
+    categories.delete("");
+    if (categories.size === 0) {
+        return false;
+    }
+    if (document.getElementById("appBar").firstElementChild.lastElementChild.id !== "category") {
+        document.getElementById("appBar").firstElementChild.insertAdjacentHTML("beforeend", "<select id='category' onchange='setFilter(this)'><option value='' selected hidden disabled>No Filter</option><option value=''>All</select>")
+
+        categories.forEach(category => {
+            document.getElementById("category").insertAdjacentHTML("beforeend", `<option>${category}</option>`);
+        });
+    }
+
+}
+
 function save(name) {
     let texts = [];
+    let categories = [];
     let elements = document.querySelectorAll(".editBox");
     elements.forEach(box => {
-        texts.push(box.innerText.slice(0,-1));//selezione del testo senza l'ultimo \n
+        if(box.innerText.split(/\r\n|\r|\n/).length>1){
+            texts.push(box.innerText.slice(0,-1));
+        }
+        else{
+            texts.push(box.innerText);
+        }
     });
-    //console.log(texts);
+
+    elements = document.querySelectorAll("[name='category']");
+    elements.forEach(category => {
+        categories.push(category.value);
+    });
+
+    texts = texts.join("");
+    texts += "";
+    texts += categories.join("");
+
+    console.log(texts);
+
     if (name !== "") {
-        download(`${name}.dhn`, texts.join(""));
+        download(`${name}.dhn`, texts);
     } else {
-        download(`untitled.dhn`, texts.join(""));
+        download(`untitled.dhn`, texts);
     }
 }
 
@@ -139,25 +198,29 @@ document.getElementById('inputfile').addEventListener('change', function () {
 
     fr.onload = function () {
         document.getElementById("app").innerHTML = '';
-        let buffer="";
+        let buffer = "";
         let text = fr.result;
-        let texts = text.split("");
-        texts.forEach(content => {
-            let lines = content.split("\n");
-            lines.forEach(line => {
-                buffer+=line+"<br>";
-            });
-            add(buffer);
-            buffer="";
-        });
+        let parts = text.split("")
+        let texts = parts[0].split("");
+        let categories = parts[1].split("");
+
+        for (let i = 0; i < texts.length; i++) {
+            let lines = texts[i].split("\n");
+            let category = categories[i];
+
+            for (let i = 0; i < lines.length; i++) {
+                buffer += lines[i] + "<br>";
+            }
+
+            add(category, buffer);
+            buffer = "";
+        }
     }
 
     fr.readAsText(this.files[0]);
-    document.getElementById("name").value=this.files[0].name.slice(0,-4);
+    document.getElementById("name").value = this.files[0].name.slice(0, -4);
     //console.log(this.files[0].name);
 })
 
 //per il primo .box
-add("");
-
-
+add("", "");
